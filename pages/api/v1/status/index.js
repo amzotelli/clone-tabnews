2b04f97878b5896f98c616bc708ec57ba46599cd
Amanda Zotelli <amzotelli@gmail.com>
2025-05-09 00:17:04 +0000

    add `jsonconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1+1 as sum;");
-  console.log(process.env.POSTGRES_DB);
   response.status(200).json({
     message: "ok",
   });
