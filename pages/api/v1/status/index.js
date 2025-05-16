3f30ae4c7d5e7c994b2a4ec6595d20c1ed3a3305
Amanda Zotelli <amzotelli@gmail.com>
2025-05-16 00:53:16 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
+  const updatedAt = new Date().toISOString();
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnectionsValue = dbOpenConnections.rows[0].count;
+
   response.status(200).json({
-    message: "ok",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
   });
 }
 
